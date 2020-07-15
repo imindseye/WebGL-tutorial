e1a8338603ba6136528513ed953aa18fbd5f7473
Han-Wei Shen <59631197+imindseye@users.noreply.github.com>
2020-07-15 13:44:52 -0400

    Add files via upload

---
+++ texture.js
@@ -0,0 +1,342 @@
+
+//////////////////////////////////////////////////////////////////
+//
+//  An example that shows you how to do 2D texture mapping 
+//  Make sure you change the browser setting so that you can open a local file 
+//
+//  Han-Wei Shen (shen.94@osu.edu)
+//
+
+var gl;
+var shaderProgram;
+var draw_type=2;
+var use_texture = 0; 
+
+ // set up the parameters for lighting 
+  var light_ambient = [0,0,0,1]; 
+  var light_diffuse = [.8,.8,.8,1];
+  var light_specular = [1,1,1,1]; 
+  var light_pos = [0,0,0,1];   // eye space position 
+
+  var mat_ambient = [0, 0, 0, 1]; 
+  var mat_diffuse= [1, 1, 0, 1]; 
+  var mat_specular = [.9, .9, .9,1]; 
+  var mat_shine = [50]; 
+
+
+//////////// Init OpenGL Context etc. ///////////////
+
+    function initGL(canvas) {
+        try {
+            gl = canvas.getContext("experimental-webgl");
+            gl.viewportWidth = canvas.width;
+            gl.viewportHeight = canvas.height;
+        } catch (e) {
+        }
+        if (!gl) {
+            alert("Could not initialise WebGL, sorry :-(");
+        }
+    }
+
+//////////////////////////////////////////////////////////////////////////////
+
+var sampleTexture; 
+
+function initTextures() {
+    sampleTexture = gl.createTexture();
+    sampleTexture.image = new Image();
+    sampleTexture.image.onload = function() { handleTextureLoaded(sampleTexture); }
+    sampleTexture.image.src = "earth.png";
+    console.log("loading texture....") 
+}
+
+function handleTextureLoaded(texture) {
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, texture.image);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+    gl.bindTexture(gl.TEXTURE_2D, null);
+}
+
+/////////////////////////////////////////////////////////////////////////////
+
+var squareVertexPositionBuffer;
+var squareVertexNormalBuffer;
+var squareVertexColorBuffer;
+var squareVertexIndexBuffer;
+
+var sqvertices = [];
+var sqnormals = []; 
+var sqindices = [];
+var sqcolors = [];
+var sqTexCoords=[]; 
+
+function InitSquare()
+{
+        sqvertices = [
+             0.5,  0.5,  0,
+	    -0.5,  0.5,  0, 
+	    - 0.5, -0.5, 0,
+ 	    0.5, -0.5,  0,
+        ];
+	sqindices = [0,1,2, 0,2,3]; 
+        sqcolors = [
+            1.0, 0.0, 0.0, 1.0,
+            0.0, 1.0, 0.0, 1.0,
+            0.0, 0.0, 1.0, 1.0,
+            1.0, 0.0, 0.0, 1.0,
+        ];    
+        sqnormals = [
+            0.0, 0.0, 1.0,
+            0.0, 0.0, 1.0,
+            0.0, 0.0, 1.0,
+            0.0, 0.0, 1.0,
+        ];    
+        sqTexCoords = [0.0,0.0,1.0,0.0,1.0,1.0,0.0,1.0]; 
+}
+
+
+function initSQBuffers() {
+
+        InitSquare(); 
+        squareVertexPositionBuffer = gl.createBuffer();
+        gl.bindBuffer(gl.ARRAY_BUFFER, squareVertexPositionBuffer);
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(sqvertices), gl.STATIC_DRAW);
+        squareVertexPositionBuffer.itemSize = 3;
+        squareVertexPositionBuffer.numItems = 4;
+
+        squareVertexNormalBuffer = gl.createBuffer();
+        gl.bindBuffer(gl.ARRAY_BUFFER, squareVertexNormalBuffer);
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(sqnormals), gl.STATIC_DRAW);
+        squareVertexNormalBuffer.itemSize = 3;
+        squareVertexNormalBuffer.numItems = 4; 
+
+        squareVertexTexCoordsBuffer = gl.createBuffer();
+        gl.bindBuffer(gl.ARRAY_BUFFER, squareVertexTexCoordsBuffer);
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(sqTexCoords), gl.STATIC_DRAW);
+        squareVertexTexCoordsBuffer.itemSize = 2;
+        squareVertexTexCoordsBuffer.numItems = 4; 
+
+	squareVertexIndexBuffer = gl.createBuffer();	
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, squareVertexIndexBuffer); 
+        gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(sqindices), gl.STATIC_DRAW);  
+        squareVertexIndexBuffer.itemsize = 1;
+        squareVertexIndexBuffer.numItems = 6;  
+
+        squareVertexColorBuffer = gl.createBuffer();
+        gl.bindBuffer(gl.ARRAY_BUFFER, squareVertexColorBuffer);
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(sqcolors), gl.STATIC_DRAW);
+        squareVertexColorBuffer.itemSize = 4;
+        squareVertexColorBuffer.numItems = 4;
+
+}
+
+/////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////
+
+    var mMatrix = mat4.create();  // model matrix
+    var vMatrix = mat4.create(); // view matrix
+    var pMatrix = mat4.create();  //projection matrix
+    var nMatrix = mat4.create();  // normal matrix
+    var Z_angle = 0.0;
+
+    function setMatrixUniforms() {
+        gl.uniformMatrix4fv(shaderProgram.mMatrixUniform, false, mMatrix);
+        gl.uniformMatrix4fv(shaderProgram.vMatrixUniform, false, vMatrix);
+        gl.uniformMatrix4fv(shaderProgram.pMatrixUniform, false, pMatrix);
+        gl.uniformMatrix4fv(shaderProgram.nMatrixUniform, false, nMatrix);	
+    }
+
+     function degToRad(degrees) {
+        return degrees * Math.PI / 180;
+     }
+
+    ///////////////////////////////////////////////////////////////
+
+    function drawScene() {
+        gl.viewport(0, 0, gl.viewportWidth, gl.viewportHeight);
+        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+	pMatrix = mat4.perspective(60, 1.0, 0.1, 100, pMatrix);  // set up the projection matrix 
+
+	vMatrix = mat4.lookAt([0,0,2], [0,0,0], [0,1,0], vMatrix);	// set up the view matrix, multiply into the modelview matrix
+
+        mat4.identity(mMatrix);	
+	
+//      console.log('Z angle = '+ Z_angle); 
+        mMatrix = mat4.rotate(mMatrix, degToRad(Z_angle), [0, 1, 1]);   // now set up the model matrix
+
+
+	mat4.identity(nMatrix); 
+	nMatrix = mat4.multiply(nMatrix, vMatrix);
+	nMatrix = mat4.multiply(nMatrix, mMatrix); 	
+	nMatrix = mat4.inverse(nMatrix);
+	nMatrix = mat4.transpose(nMatrix); 
+
+        shaderProgram.light_posUniform = gl.getUniformLocation(shaderProgram, "light_pos");
+
+
+	gl.uniform4f(shaderProgram.light_posUniform,light_pos[0], light_pos[1], light_pos[2], light_pos[3]); 	
+	gl.uniform4f(shaderProgram.ambient_coefUniform, mat_ambient[0], mat_ambient[1], mat_ambient[2], 1.0); 
+	gl.uniform4f(shaderProgram.diffuse_coefUniform, mat_diffuse[0], mat_diffuse[1], mat_diffuse[2], 1.0); 
+	gl.uniform4f(shaderProgram.specular_coefUniform, mat_specular[0], mat_specular[1], mat_specular[2],1.0); 
+	gl.uniform1f(shaderProgram.shininess_coefUniform, mat_shine[0]); 
+
+	gl.uniform4f(shaderProgram.light_ambientUniform, light_ambient[0], light_ambient[1], light_ambient[2], 1.0); 
+	gl.uniform4f(shaderProgram.light_diffuseUniform, light_diffuse[0], light_diffuse[1], light_diffuse[2], 1.0); 
+	gl.uniform4f(shaderProgram.light_specularUniform, light_specular[0], light_specular[1], light_specular[2],1.0); 
+
+        gl.bindBuffer(gl.ARRAY_BUFFER, squareVertexPositionBuffer);
+        gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, squareVertexPositionBuffer.itemSize, gl.FLOAT, false, 0, 0);
+
+        gl.bindBuffer(gl.ARRAY_BUFFER, squareVertexNormalBuffer);
+	gl.vertexAttribPointer(shaderProgram.vertexNormalAttribute, squareVertexNormalBuffer.itemSize, gl.FLOAT, false, 0, 0);
+
+        gl.bindBuffer(gl.ARRAY_BUFFER, squareVertexTexCoordsBuffer);
+	gl.vertexAttribPointer(shaderProgram.vertexTexCoordsAttribute, squareVertexTexCoordsBuffer.itemSize, gl.FLOAT, false, 0, 0);
+
+        gl.bindBuffer(gl.ARRAY_BUFFER, squareVertexColorBuffer);
+        gl.vertexAttribPointer(shaderProgram.vertexColorAttribute,squareVertexColorBuffer.itemSize, gl.FLOAT, false, 0, 0);
+
+	// draw elementary arrays - triangle indices 
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, squareVertexIndexBuffer);
+
+	setMatrixUniforms();   // pass the modelview mattrix and projection matrix to the shader 
+	gl.uniform1i(shaderProgram.use_textureUniform, use_texture); 
+
+        gl.activeTexture(gl.TEXTURE0);   // set texture unit 0 to use 
+	gl.bindTexture(gl.TEXTURE_2D, sampleTexture);    // bind the texture object to the texture unit 
+	gl.uniform1i(shaderProgram.textureUniform, 0);   // pass the texture unit to the shader 
+
+	if (draw_type ==1) gl.drawArrays(gl.LINE_LOOP, 0, squareVertexPositionBuffer.numItems);	
+        else if (draw_type ==0) gl.drawArrays(gl.POINTS, 0, squareVertexPositionBuffer.numItems);
+	else if (draw_type==2) gl.drawElements(gl.TRIANGLES, squareVertexIndexBuffer.numItems , gl.UNSIGNED_SHORT, 0);
+	
+    }
+
+
+    ///////////////////////////////////////////////////////////////
+
+     var lastMouseX = 0, lastMouseY = 0;
+
+    ///////////////////////////////////////////////////////////////
+
+     function onDocumentMouseDown( event ) {
+          event.preventDefault();
+          document.addEventListener( 'mousemove', onDocumentMouseMove, false );
+          document.addEventListener( 'mouseup', onDocumentMouseUp, false );
+          document.addEventListener( 'mouseout', onDocumentMouseOut, false );
+          var mouseX = event.clientX;
+          var mouseY = event.clientY;
+
+          lastMouseX = mouseX;
+          lastMouseY = mouseY; 
+
+      }
+
+     function onDocumentMouseMove( event ) {
+          var mouseX = event.clientX;
+          var mouseY = event.ClientY; 
+
+          var diffX = mouseX - lastMouseX;
+          var diffY = mouseY - lastMouseY;
+
+          Z_angle = Z_angle + diffX/5;
+
+          lastMouseX = mouseX;
+          lastMouseY = mouseY;
+
+          drawScene();
+     }
+
+     function onDocumentMouseUp( event ) {
+          document.removeEventListener( 'mousemove', onDocumentMouseMove, false );
+          document.removeEventListener( 'mouseup', onDocumentMouseUp, false );
+          document.removeEventListener( 'mouseout', onDocumentMouseOut, false );
+     }
+
+     function onDocumentMouseOut( event ) {
+          document.removeEventListener( 'mousemove', onDocumentMouseMove, false );
+          document.removeEventListener( 'mouseup', onDocumentMouseUp, false );
+          document.removeEventListener( 'mouseout', onDocumentMouseOut, false );
+     }
+
+    ///////////////////////////////////////////////////////////////
+
+    function webGLStart() {
+        var canvas = document.getElementById("code10-canvas");
+        initGL(canvas);
+        initShaders();
+
+	gl.enable(gl.DEPTH_TEST); 
+
+        shaderProgram.vertexPositionAttribute = gl.getAttribLocation(shaderProgram, "aVertexPosition");
+        gl.enableVertexAttribArray(shaderProgram.vertexPositionAttribute);
+
+        shaderProgram.vertexNormalAttribute = gl.getAttribLocation(shaderProgram, "aVertexNormal");
+        gl.enableVertexAttribArray(shaderProgram.vertexNormalAttribute);
+
+        shaderProgram.vertexTexCoordsAttribute = gl.getAttribLocation(shaderProgram, "aVertexTexCoords");
+        gl.enableVertexAttribArray(shaderProgram.vertexTexCoordsAttribute);
+	
+        shaderProgram.vertexColorAttribute = gl.getAttribLocation(shaderProgram, "aVertexColor");
+        gl.enableVertexAttribArray(shaderProgram.vertexColorAttribute);
+	
+        shaderProgram.mMatrixUniform = gl.getUniformLocation(shaderProgram, "uMMatrix");
+        shaderProgram.vMatrixUniform = gl.getUniformLocation(shaderProgram, "uVMatrix");
+	shaderProgram.pMatrixUniform = gl.getUniformLocation(shaderProgram, "uPMatrix");
+	shaderProgram.nMatrixUniform = gl.getUniformLocation(shaderProgram, "uNMatrix");	
+
+        shaderProgram.light_posUniform = gl.getUniformLocation(shaderProgram, "light_pos");
+        shaderProgram.ambient_coefUniform = gl.getUniformLocation(shaderProgram, "ambient_coef");	
+        shaderProgram.diffuse_coefUniform = gl.getUniformLocation(shaderProgram, "diffuse_coef");
+        shaderProgram.specular_coefUniform = gl.getUniformLocation(shaderProgram, "specular_coef");
+        shaderProgram.shininess_coefUniform = gl.getUniformLocation(shaderProgram, "mat_shininess");
+
+        shaderProgram.light_ambientUniform = gl.getUniformLocation(shaderProgram, "light_ambient");	
+        shaderProgram.light_diffuseUniform = gl.getUniformLocation(shaderProgram, "light_diffuse");
+        shaderProgram.light_specularUniform = gl.getUniformLocation(shaderProgram, "light_specular");	
+
+        shaderProgram.textureUniform = gl.getUniformLocation(shaderProgram, "myTexture");
+        shaderProgram.use_textureUniform = gl.getUniformLocation(shaderProgram, "use_texture");
+
+        initSQBuffers();
+
+	initTextures(); 
+
+        gl.clearColor(0.0, 0.0, 0.0, 1.0);
+        console.log('start! ');
+        console.error('I hope no error ....');
+
+       document.addEventListener('mousedown', onDocumentMouseDown,
+       false); 
+
+        drawScene();
+    }
+
+function BG(red, green, blue) {
+
+    gl.clearColor(red, green, blue, 1.0);
+    drawScene(); 
+
+} 
+
+function redraw() {
+    Z_angle = 0; 
+    drawScene();
+}
+    
+
+function geometry(value) {
+
+    draw_type = value;
+    drawScene();
+
+} 
+
+function texture(value) {
+
+    use_texture = value;
+    drawScene();
+
+} 
