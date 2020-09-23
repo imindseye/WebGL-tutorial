87fc6a26353386586385938e3186712340ff0f52
Han-Wei Shen <59631197+imindseye@users.noreply.github.com>
2020-09-22 22:11:51 -0400

    Add files via upload

---
+++ uniform-color-xform.js
@@ -0,0 +1,223 @@
+
+////////////////////////////////////////////////////////
+//
+//  Simple transformation of 2D triangles 
+//  Notice TRIANGLE_FAN is used to draw 
+// 
+//  Han-Wei Shen (shen.94@osu.edu)
+//
+var gl;
+var shaderProgram;
+var draw_type=2; 
+
+//////////// Init OpenGL Context etc. ///////////////
+
+    function initGL(canvas) {
+        try {
+            gl = canvas.getContext("experimental-webgl");
+            gl.viewportWidth = canvas.width;
+            gl.viewportHeight = canvas.height;
+        } catch (e) {
+        }
+        if (!gl) {
+            alert("Could not initialise WebGL, sorry :-(");
+        }
+    }
+
+
+
+
+    ///////////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////
+
+    var squareVertexPositionBuffer;
+    var squareVertexColorBuffer;
+
+   ////////////////    Initialize VBO  ////////////////////////
+
+    function initBuffers() {
+
+        squareVertexPositionBuffer = gl.createBuffer();
+        gl.bindBuffer(gl.ARRAY_BUFFER, squareVertexPositionBuffer);
+        vertices = [
+            0.5,  0.5,  0.0,
+		    -0.5,  0.5,  0.0, 
+            -0.5, -0.5,  0.0,
+	        0.5, -0.5,  0.0,
+
+        ];
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+        squareVertexPositionBuffer.vertexSize = 3;
+        squareVertexPositionBuffer.numVertices = 4;
+
+    }
+
+    ///////////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////
+
+
+    var mvMatrix = mat4.create();
+    var Z_angle = 0.0;
+
+     function degToRad(degrees) {
+        return degrees * Math.PI / 180;
+     }
+
+    ///////////////////////////////////////////////////////////////
+
+    function drawScene() {
+        gl.viewport(0, 0, gl.viewportWidth, gl.viewportHeight);
+        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+        mat4.identity(mvMatrix);
+        console.log('Z angle = '+ Z_angle); 
+        mvMatrix = mat4.translate(mvMatrix, [-0.5, 0, 0]); 
+        mvMatrix = mat4.rotate(mvMatrix, degToRad(Z_angle), [0, 0, 1]); 
+        mvMatrix = mat4.scale(mvMatrix, [0.2, 0.2, 0.2]); 
+
+        var offset = 0; 
+        var stride = 0; 
+
+        gl.bindBuffer(gl.ARRAY_BUFFER, squareVertexPositionBuffer);
+        gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, squareVertexPositionBuffer.vertexSize, gl.FLOAT, false, stride, offset);
+
+       var color = [1.0, 0.0, 0.0, 1.0]; 
+
+       gl.uniformMatrix4fv(shaderProgram.mvMatrixUniform, false, mvMatrix);
+       gl.uniform4fv(shaderProgram.uColor, color); 
+
+	    if (draw_type==2) gl.drawArrays(gl.TRIANGLE_FAN, 0, squareVertexPositionBuffer.numVertices);
+	    else if (draw_type ==1) gl.drawArrays(gl.LINE_LOOP, 0, squareVertexPositionBuffer.numVertices);	
+        else if (draw_type ==0) gl.drawArrays(gl.POINTS, 0, squareVertexPositionBuffer.numVertices);
+
+        // draw the second square. Note I am re-using my previous vbo 
+        mat4.identity(mvMatrix);
+        console.log('Z angle = '+ Z_angle); 
+        mvMatrix = mat4.translate(mvMatrix, [0.5,0,0]); 
+        mvMatrix = mat4.rotate(mvMatrix, -degToRad(Z_angle), [0, 0, 1]); 
+        mvMatrix = mat4.scale(mvMatrix, [0.2, 0.2, 0.2]); 
+
+
+        color = [1,1,0, 1]; 
+        gl.uniformMatrix4fv(shaderProgram.mvMatrixUniform, false, mvMatrix);
+        gl.uniform4fv(shaderProgram.uColor, color); 
+
+        if (draw_type==2) gl.drawArrays(gl.TRIANGLE_FAN, 0, squareVertexPositionBuffer.numVertices);
+	    else if (draw_type ==1) gl.drawArrays(gl.LINE_LOOP, 0, squareVertexPositionBuffer.numVertices);	
+        else if (draw_type ==0) gl.drawArrays(gl.POINTS, 0, squareVertexPositionBuffer.numVertices);
+
+    }
+
+
+    ///////////////////////////////////////////////////////////////
+
+     var lastMouseX = 0, lastMouseY = 0;
+
+    ///////////////////////////////////////////////////////////////
+
+     function onDocumentMouseDown( event ) {
+          event.preventDefault();
+          document.addEventListener( 'mousemove', onDocumentMouseMove, false );
+          document.addEventListener( 'mouseup', onDocumentMouseUp, false );
+          document.addEventListener( 'mouseout', onDocumentMouseOut, false );
+          var mouseX = event.clientX;
+          var mouseY = event.clientY;
+
+          lastMouseX = mouseX;
+          lastMouseY = mouseY; 
+
+      }
+
+     function onDocumentMouseMove( event ) {
+          var mouseX = event.clientX;
+          var mouseY = event.ClientY; 
+
+          var diffX = mouseX - lastMouseX;
+          var diffY = mouseY - lastMouseY;
+
+          Z_angle = Z_angle + diffX/5;
+
+          lastMouseX = mouseX;
+          lastMouseY = mouseY;
+
+          drawScene();
+     }
+
+     function onDocumentMouseUp( event ) {
+          document.removeEventListener( 'mousemove', onDocumentMouseMove, false );
+          document.removeEventListener( 'mouseup', onDocumentMouseUp, false );
+          document.removeEventListener( 'mouseout', onDocumentMouseOut, false );
+     }
+
+     function onDocumentMouseOut( event ) {
+          document.removeEventListener( 'mousemove', onDocumentMouseMove, false );
+          document.removeEventListener( 'mouseup', onDocumentMouseUp, false );
+          document.removeEventListener( 'mouseout', onDocumentMouseOut, false );
+     }
+
+    ///////////////////////////////////////////////////////////////
+
+    function webGLStart() {
+        var canvas = document.getElementById("code03-canvas");
+        initGL(canvas);
+        initShaders();
+
+        shaderProgram.vertexPositionAttribute = gl.getAttribLocation(shaderProgram, "aVertexPosition");
+        gl.enableVertexAttribArray(shaderProgram.vertexPositionAttribute);
+
+        shaderProgram.mvMatrixUniform = gl.getUniformLocation(shaderProgram, "uMVMatrix");
+        shaderProgram.uColor = gl.getUniformLocation(shaderProgram, "uColor");
+
+        initBuffers(); 
+
+        gl.clearColor(0.0, 0.0, 0.0, 1.0);
+
+       document.addEventListener('mousedown', onDocumentMouseDown,
+       false); 
+       document.addEventListener('keydown', onKeyDown, false);
+
+
+        drawScene();
+    }
+
+function BG(red, green, blue) {
+
+    gl.clearColor(red, green, blue, 1.0);
+    drawScene(); 
+
+} 
+
+function redraw() {
+    Z_angle = 0; 
+    drawScene();
+}
+    
+
+function geometry(type) {
+
+    draw_type = type;
+    drawScene();
+
+} 
+///////////////////////////////////////////////////////////////////////////
+//
+//  key stroke handler 
+//
+    function onKeyDown(event) {
+      console.log(event.keyCode);
+      switch(event.keyCode)  {
+
+         case 82:
+              if (event.shiftKey) {
+                  console.log('enter R');
+                  Z_angle +=6; 
+              }
+              else {
+          console.log('enter r');
+                  Z_angle -=6; 
+              }
+          break;
+      }
+
+	drawScene();	 // draw the VBO 
+    }
